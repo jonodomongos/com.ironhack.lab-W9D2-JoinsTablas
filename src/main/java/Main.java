e37a83e0790166bbedccc5cdf7c957ee6daf92d4
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 16:16:24 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+    1. Crea tres tablas relacionadas entre ellas. Inserta algunos datos, pongamos 5 registros en cada tabla, asegurándote que hay NULLs en algún caso en todas las columnas posibles. Te ayudará a esto si no tienes un campo id autoincremental y not null.
+    Prueba a realizar INNER JOIN, LEFT JOIN y RIGHT JOIN con estas tablas. Obtienes los resultados esperados?
+
+    Si te parece muy complicado, puedes ir al video:
+    https://www.youtube.com/watch?v=NMKqPPNcNrk
+
+    Crea las tablas que allí se explican, siguiendo los JOINS del video (incluye los códigos en el comentario del vídeo).
+    Después puedes ampliarlo con su siguiente video:
+    https://www.youtube.com/watch?v=rO8OoVA9YEY
+
+    2. EXTRA (opcional):
+    Ve a esta web:
+    https://www.w3resource.com/mysql-exercises/join-exercises/index.php
+    Donde encontrarás algunos ejercicios para hacer JOINS (contiene soluciones). No tienes por qué usar las instrucciones que ellos proponen, mientras obtengas el mismo resultado.
+    Esta página contiene un editor online abajo de todo para que no tengas que bajarte las bases de datos.
+    */
+    }
+}
